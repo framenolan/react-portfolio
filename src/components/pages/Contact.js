ccbf65ff7eab5cd9e41c1dd3e475b3a9a8242483
Nolan Frame <frame.nolan@gmail.com>
2022-07-24 14:06:47 -0700

    added email address to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,14 +6,17 @@ export default function Contact() {
     return (
         <section className="Contact">
             <h2>Contact</h2>
-            <div className='ContactCard'>
-                <a href='mailto:frame.nolan@gmail.com?subject=Portfolio Inquiry' target="_blank" rel="noreferrer">Email Me</a>
-            </div>
-            <div className='ContactCard'>
-                <a href='https://www.linkedin.com/in/framenolan/' target="_blank" rel="noreferrer">LinkedIn</a>
-            </div>
-            <div className='ContactCard'>
-                <a href='https://github.com/framenolan' target="_blank" rel="noreferrer">GitHub</a>
+            <p className="EmailText">Please feel free to contact me by email at <a href={"mailto:frame.nolan@gmail.com"} id="emailContact" >frame.nolan@gmail.com</a>.</p>
+            <div className="CardContainer">
+                <div className='ContactCard'>
+                    <a href='mailto:frame.nolan@gmail.com?subject=Portfolio Inquiry' target="_blank" rel="noreferrer">Email Me</a>
+                </div>
+                <div className='ContactCard'>
+                    <a href='https://www.linkedin.com/in/framenolan/' target="_blank" rel="noreferrer">LinkedIn</a>
+                </div>
+                <div className='ContactCard'>
+                    <a href='https://github.com/framenolan' target="_blank" rel="noreferrer">GitHub</a>
+                </div>
             </div>
         </section>
     )
