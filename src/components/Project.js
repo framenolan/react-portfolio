04b89ee2e1a8c1354001302a43e133c2204b96a8
Nolan Frame <frame.nolan@gmail.com>
2022-07-24 14:53:42 -0700

    tinkering with descriptions

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,14 +7,19 @@ export default function Project(props) {
 
     return (
         <div className='Project'>
-            <div className='ProjectHeader'>
-                <p className='ProjectName'>{props.name}</p>
-                <ul>
-                    <a href={props.repo} className='RepoLink' target="_blank" rel="noreferrer"><img src={wgithub} height="32px" alt='icon to open repo'></img></a>
-                    <a href={props.url} className='DeployedLink' target="_blank" rel="noreferrer"><img src={wplay} height="32px" width="32px" alt='icon to open deployed app'></img></a>
-                </ul>
+            <div className='ProjectContainer'>
+                <div className='ProjectHeader'>
+                    <p className='ProjectName'>{props.name}</p>
+                    <ul>
+                        <a href={props.repo} className='RepoLink' target="_blank" rel="noreferrer"><img src={wgithub} height="32px" alt='icon to open repo'></img></a>
+                        <a href={props.url} className='DeployedLink' target="_blank" rel="noreferrer"><img src={wplay} height="32px" width="32px" alt='icon to open deployed app'></img></a>
+                    </ul>
+                </div>
+                <div className='Background' style={{ backgroundImage: `url(${props.image})` }}>
+                </div>
             </div>
-            <div className='Background' style={{ backgroundImage: `url(${props.image})` }}>
+            <div className='DescriptionContainer'>
+                <p className='ProjectDescription'>{props.description}</p>
             </div>
         </div>
     )
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,13 +13,14 @@ export default function Portfolio() {
     return (
         <section className="Portfolio">
             <h2>Portfolio</h2>
-            <Project name="Where's the Beer?" image={Wheres} url="https://framenolan.github.io/wheres-the-beer/" repo="https://github.com/framenolan/wheres-the-beer"/>
-            <Project name="Star Arcade" image={Star} url="https://kooler-games.herokuapp.com/" repo="https://github.com/kayjinyi/cool-game/"/>
-            <Project name="Buddygram" image={BuddyGram} url="https://safe-mesa-41776.herokuapp.com/login" repo="https://github.com/framenolan/buddygram"/>
+            {/* <Project name="Where's the Beer?" image={Wheres} url="https://framenolan.github.io/wheres-the-beer/" repo="https://github.com/framenolan/wheres-the-beer" description="A brewery directory app with an API to Google Maps"/> */}
+            <Project name="Where's the Beer?" image={Wheres} url="https://framenolan.github.io/wheres-the-beer/" repo="https://github.com/framenolan/wheres-the-beer" description=""/>
+            <Project name="Star Arcade" image={Star} url="https://kooler-games.herokuapp.com/" repo="https://github.com/kayjinyi/cool-game/" description=""/>
+            <Project name="Buddygram" image={BuddyGram} url="https://safe-mesa-41776.herokuapp.com/login" repo="https://github.com/framenolan/buddygram" description=""/>
             {/* <Project name="E-Commerce Backend" url="https://enigmatic-tor-94718.herokuapp.com/" repo="https://github.com/framenolan/ecommerce-backend"/> */}
-            <Project name="Weather Dashboard" image={Weather} url="https://framenolan.github.io/weather-dashboard/" repo="https://github.com/framenolan/weather-dashboard"/>
-            <Project name="Team Profile Generator" image={Team} url="https://github.com/framenolan/team-profile-generator" repo="https://github.com/framenolan/team-profile-generator"/>
-            <Project name="Workday Scheduler" image={Workday} url="https://framenolan.github.io/workday-scheduler/" repo="https://github.com/framenolan/workday-scheduler"/>
+            <Project name="Weather Dashboard" image={Weather} url="https://framenolan.github.io/weather-dashboard/" repo="https://github.com/framenolan/weather-dashboard" description=""/>
+            <Project name="Team Profile Generator" image={Team} url="https://github.com/framenolan/team-profile-generator" repo="https://github.com/framenolan/team-profile-generator" description=""/>
+            <Project name="Workday Scheduler" image={Workday} url="https://framenolan.github.io/workday-scheduler/" repo="https://github.com/framenolan/workday-scheduler" description=""/>
         </section>
     )
 }
